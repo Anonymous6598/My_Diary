5f69acb5c41d926a9d6c057516e29e9fde75b4c1
Виктор Илић <121385046+Anonymous6598@users.noreply.github.com>
2025-02-22 01:02:02 +0100

    Delete My_Diary directory

--- My_Diary/My_Diary_window.py
+++
@@ -1,8 +0,0 @@
-import typing
-from customtkinter import *
-from tkinterdnd2 import *
-
-class Tk(CTk, TkinterDnD.DnDWrapper):
-    def __init__(self: typing.Self, *args, **kwargs):
-        CTk.__init__(self, *args, **kwargs)
-        self.TkdndVersion = TkinterDnD._require(self)
--- My_Diary/tkdnd2.8/pkgIndex.tcl
+++
@@ -1,6 +0,0 @@
-#
-# Tcl package index file
-#
-package ifneeded tkdnd 2.8 \
-  "source \{$dir/tkdnd.tcl\} ; \
-   tkdnd::initialise \{$dir\} tkdnd28.dll tkdnd"
