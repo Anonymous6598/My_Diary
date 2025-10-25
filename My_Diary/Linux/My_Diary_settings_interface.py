79f67a3df092e7ca1e46bc88871048fa98f10fdd
Viktor Ilić <vikiilitch@outlook.com>
2025-10-25 14:50:50 +0200

    Create My_Diary_settings_interface.py

---
+++ My_Diary/Linux/My_Diary_settings_interface.py
@@ -0,0 +1,11 @@
+import abc, typing
+
+class My_Diary_setting_window_interface(abc.ABC):
+
+    @abc.abstractmethod
+    def __language_settings__(self: typing.Self, pickle_serializer: pickle) -> None:
+        pass
+
+    @abc.abstractmethod
+    def __theme_settings__(self: typing.Self, pickle_serializer: pickle) -> None:
+        pass
