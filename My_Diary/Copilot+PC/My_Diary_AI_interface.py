e56e785522954b3dc01dc10994ab6c437157701d
Viktor Ilić <vikiilitch@outlook.com>
2025-10-25 18:30:19 +0200

    Update My_Diary_AI_interface.py

--- My_Diary/Copilot+PC/My_Diary_AI_interface.py
+++ My_Diary/Copilot+PC/My_Diary_AI_interface.py
@@ -1,7 +1,11 @@
-import abc, typing
+import abc, typing, openvino_genai
 
 class My_Diary_AI_interface(abc.ABC):
 
+    @abc.abstractmethod
+    def __initialize_model__(self: typing.Self) -> openvino_genai.LLMPipeline:
+        pass
+    
     @abc.abstractmethod
     def __response__(self: typing.Self, prompt: str) -> str:
         pass
