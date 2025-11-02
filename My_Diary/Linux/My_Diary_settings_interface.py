c069a499166767da939a955922a09c725984cb1e
Viktor Ilić <vikiilitch@outlook.com>
2025-11-02 13:32:37 +0100

    Update My_Diary_settings_interface.py

--- My_Diary/Linux/My_Diary_settings_interface.py
+++ My_Diary/Linux/My_Diary_settings_interface.py
@@ -1,4 +1,4 @@
-import abc, typing
+import abc, typing, pickle
 
 class My_Diary_setting_window_interface(abc.ABC):
 
@@ -9,3 +9,4 @@ class My_Diary_setting_window_interface(abc.ABC):
     @abc.abstractmethod
     def __theme_settings__(self: typing.Self, pickle_serializer: pickle) -> None:
         pass
+
